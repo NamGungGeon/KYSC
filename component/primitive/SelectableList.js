d8cdb74da39335e5ed97ff04cba723fb3582e0b2
namgung-geon <rndrjs123@naver.com>
2020-08-22 22:12:51 +0900

    InitialSettingScreen is added

---
+++ component/primitive/SelectableList.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+import MyColors from '../resources/colors/colors';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: '100%',
+    backgroundColor: 'white',
+  },
+  row: {
+    padding: 8,
+    color: '#333333',
+    backgroundColor: 'white',
+    width: '100%',
+  },
+  selected: {},
+  list: {},
+});
+const SelectableList = ({list = [], defaultSelect, onSelect}) => {
+  const [selected, setSelected] = useState(defaultSelect);
+  return (
+    <View style={styles.wrapper}>
+      {list.map((entity) => {
+        return (
+          <Pressable
+            key={entity.value}
+            onPress={() => {
+              if (onSelect) onSelect(entity);
+              console.log(entity);
+              setSelected(entity);
+            }}>
+            <Text
+              style={{
+                ...styles.row,
+                color:
+                  selected.value === entity.value ? MyColors.primary : 'black',
+              }}>
+              {entity.title}
+            </Text>
+          </Pressable>
+        );
+      })}
+    </View>
+  );
+};
+
+export default SelectableList;
---
+++ component/screens/InitialSettings.js
@@ -0,0 +1,167 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+  Alert,
+} from 'react-native';
+import moment from 'moment';
+import MyColors from '../resources/colors/colors';
+import {Calendar} from 'react-native-calendars';
+import SelectableList from '../primitive/SelectableList';
+import AsyncStorage from '@react-native-community/async-storage';
+import NotificationUtil from '../util/NotificationUtil';
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: MyColors.primary,
+    flex: 1,
+  },
+  section: {
+    padding: 16,
+  },
+  mainTitle: {
+    color: MyColors.white,
+    fontWeight: '600',
+    fontSize: 32,
+  },
+  title: {
+    color: MyColors.white,
+    fontWeight: '600',
+    fontSize: 24,
+  },
+  explain: {
+    color: MyColors.white,
+    fontWeight: '400',
+    fontSize: 16,
+  },
+  buttonWrapper: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: MyColors.white,
+  },
+  buttonText: {
+    padding: 16,
+    color: MyColors.primary,
+    fontWeight: '600',
+    fontSize: 16,
+  },
+});
+const InitialSettings = ({navigation}) => {
+  const currentDate = moment().format('YYYY-MM-DD');
+  const periods = [
+    {
+      title: '1 week',
+      value: 7,
+    },
+    {
+      title: '2 week',
+      value: 14,
+    },
+    {
+      title: '3 week',
+      value: 21,
+    },
+    {
+      title: '4 week',
+      value: 28,
+    },
+  ];
+  const [lastChangingDate, setLastChangingDate] = useState(currentDate);
+  const [changingPeriod, setChangingPeriod] = useState(periods[0].value);
+
+  const onSettingFinished = async () => {
+    await AsyncStorage.setItem('lastChangingDate', lastChangingDate);
+    await AsyncStorage.setItem('changingPeriod', `${changingPeriod}`);
+    Alert.alert(
+      'All Setting is finished',
+      'Now, we send to notification when your shaver knife is dirty',
+    );
+    NotificationUtil.register(
+      'Welcome!',
+      'We send to notification when your shaver knife is dirty',
+    );
+    navigation.reset({
+      index: 0,
+      routes: [{name: 'Home'}],
+    });
+  };
+
+  return (
+    <>
+      <StatusBar backgroundColor={MyColors.primary} />
+      <SafeAreaView style={styles.background}>
+        <ScrollView overScrollMode={'never'}>
+          <View>
+            <View style={styles.section}>
+              <Text style={styles.mainTitle}>Welcome to SKCN!</Text>
+              <Text style={styles.explain}>
+                This action is needed only once
+              </Text>
+            </View>
+            <View style={styles.section}>
+              <Text style={styles.title}>
+                Step 1: Choose the last date changing shaver knife
+              </Text>
+              <Text style={styles.explain}>
+                This information is used to notify the next changing day
+              </Text>
+              <View
+                style={{
+                  backgroundColor: MyColors.white,
+                }}>
+                <Calendar
+                  markedDates={{
+                    [lastChangingDate]: {
+                      selected: true,
+                      selectedColor: '#00adf5',
+                    },
+                  }}
+                  maxDate={currentDate}
+                  onDayPress={(day) => {
+                    setLastChangingDate(day.dateString);
+                    console.log(day.dateString);
+                  }}
+                  theme={{
+                    selectedDayBackgroundColor: '#00adf5',
+                  }}
+                  current={currentDate}
+                />
+              </View>
+            </View>
+            <View style={styles.section}>
+              <Text style={styles.title}>
+                Step 2: Inform changing period to us
+              </Text>
+              <Text style={styles.explain}>
+                This information is used to notify the next changing day also
+              </Text>
+              <View>
+                <SelectableList
+                  list={periods}
+                  onSelect={(period) => setChangingPeriod(period.value)}
+                  defaultSelect={periods[0]}
+                />
+              </View>
+            </View>
+            <View style={styles.section}>
+              <Text style={styles.title}>Step 3: Finish</Text>
+              <Pressable
+                style={styles.buttonWrapper}
+                onPress={onSettingFinished}>
+                <Text style={styles.buttonText}>Done!</Text>
+              </Pressable>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default InitialSettings;
