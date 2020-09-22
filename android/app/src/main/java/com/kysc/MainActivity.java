716e94ea5b53b2f15b5b47dd3b271217448d40db
namgung-geon <rndrjs123@naver.com>
2020-09-22 19:50:52 +0900

    Change android package name

--- android/app/src/main/java/com/kysc/MainActivity.java
+++ android/app/src/main/java/com/kysc/MainActivity.java
@@ -1,4 +1,4 @@
-package com.skcn;
+package com.kysc;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/kysc/MainApplication.java
+++ android/app/src/main/java/com/kysc/MainApplication.java
@@ -1,14 +1,15 @@
-package com.skcn;
+package com.kysc;
 
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.skcn.BuildConfig;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -63,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.skcn.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.kysc.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
